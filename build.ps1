a7c5640fac840d598a5f9ffc74b11860b3de111c
jmarkman <jmarkman446@gmail.com>
2025-09-20 01:31:51 -0400

    Update build script

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 $baseDir = Resolve-Path .\
 $buildDir = "$baseDir\build"
+$debugDir = "$buildDir\debug"
 $sourceDir = "$baseDir\src"
 $projectName = "CurrenSee"
 $unitTestPath = "$sourceDir\$projectName.Tests"
@@ -8,23 +9,42 @@ $dotnetCliVerbosity = "m"
 $buildConfiguration
 
 function Init {
-    Write-Output "Creating build directory"
+    Write-Output "Creating deployment directory"
     Remove-Item $buildDir -Recurse -Force -ErrorAction Ignore
+    Remove-Item $debugDir -Recurse -Force -ErrorAction Ignore
+
+    if ($buildConfiguration -eq "Debug") {
+        New-Item -Path $debugDir -ItemType Directory > $null 
+    } elseif ($buildConfiguration -eq "Release") {
+        New-Item -Path $buildDir -ItemType Directory > $null 
+    } else {
+        throw "Unknown build configuration: $buildConfiguration"
+    }
+
     New-Item -Path $buildDir -ItemType Directory > $null 
 
     Write-Output "Clean & restore solution"
-    & dotnet clean "$sourceDir\$projectName.sln" -nologo -v $dotnetCliVerbosity
-    & dotnet restore "$sourceDir\$projectName.sln" -nologo --interactive -v $dotnetCliVerbosity
+    & dotnet clean "$sourceDir\$projectName.sln" --nologo -v $dotnetCliVerbosity
+    & dotnet restore "$sourceDir\$projectName.sln" --nologo --interactive -v $dotnetCliVerbosity
 }
 
 function Compile {
     Write-Output "Compiling"
-    & dotnet build "$sourceDir\$projectName.sln" -nologo --no-restore -v $dotnetCliVerbosity --no-incremental -c $buildConfiguration
+
+    if ($buildConfiguration -ne $null) {
+        & dotnet build "$sourceDir\$projectName.sln" --nologo --no-restore -v $dotnetCliVerbosity --no-incremental -c $buildConfiguration 
+    } else {
+        & dotnet build "$sourceDir\$projectName.sln" --nologo --no-restore -v $dotnetCliVerbosity --no-incremental -c "Debug" 
+    }
 }
 
 function UnitTests {
     Write-Output "Running Unit Tests"
-    & dotnet test "$unitTestPath\$projectName.Tests.csproj" -nologo -v $dotnetCliVerbosity --no-build --no-restore -c $buildConfiguration
+    if ($buildConfiguration -ne $null) {
+        & dotnet test "$unitTestPath\$projectName.Tests.csproj" --nologo -v $dotnetCliVerbosity --no-build --no-restore -c $buildConfiguration
+    } else {
+        & dotnet test "$unitTestPath\$projectName.Tests.csproj" --nologo -v $dotnetCliVerbosity --no-build --no-restore -c "Debug"
+    }
 }
 
 function CopyOutput {
